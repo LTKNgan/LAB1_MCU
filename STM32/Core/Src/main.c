6af996c548456ae9aac30f3009d013afc8b69093
LTKNgan <ngan.lathikieu@hcmut.edu.vn>
2023-09-24 23:16:14 +0700

    proteus file for exercise 1

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -159,7 +159,8 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, RED_LED_Pin|YELLOW_LED_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, RED_LED_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, YELLOW_LED_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pins : RED_LED_Pin YELLOW_LED_Pin */
   GPIO_InitStruct.Pin = RED_LED_Pin|YELLOW_LED_Pin;
